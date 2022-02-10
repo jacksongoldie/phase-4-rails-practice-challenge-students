4f7339f3595919ec4967ff7939b388bc0680f240
root <root@LAPTOP-78CKJOIP.localdomain>
2022-02-10 14:03:56 -0600

    complete??

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :record_not_valid
+
+    private
+
+    def record_not_found
+        render json: { error: 'Record not found' }, status: :not_found
+    end
+
+    def record_not_valid(invalid)
+        render json: { error: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
 end
